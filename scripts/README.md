518b010817bd6804678a2ecbb6662bae9d0dd426
Matt Strimas-Mackey <mes335@cornell.edu>
2023-11-24 09:04:31 -0800

    more lesson 2 cleanup

---
+++ scripts/README.md
@@ -0,0 +1,1 @@
+The R scripts in this directory are template scripts intended for use during interactive R workshops covering the material in this guide.
---
+++ scripts/ebird.R
@@ -0,0 +1,149 @@
+library(auk)
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+library(lubridate)
+library(readr)
+library(sf)
+
+
+# Import eBird data ----
+
+# checklist data
+f_sed <- "data-raw/ebd_US-GA_woothr_smp_relJun-2023_sampling.txt"
+
+
+# EXERCISE: Make a histogram of the distribution of distance traveling for
+# traveling protocol checklists.
+
+
+# observation data
+f_ebd <- "data-raw/ebd_US-GA_woothr_smp_relJun-2023.txt"
+
+
+# ├ Shared checklists ----
+
+# import checklist data without collapsing shared checklists
+checklists_shared <- read_sampling(f_sed, unique = FALSE)
+# identify shared checklists
+
+# collapse shared checklists
+
+
+# ├ Taxonomic rollup ----
+
+# import one of the auk example datasets without rolling up taxonomy
+obs_ex <- system.file("extdata/ebd-rollup-ex.txt", package = "auk") %>%
+  read_ebd(rollup = FALSE)
+# rollup taxonomy
+
+# identify the taxonomic categories present in each dataset
+
+# yellow-rumped warbler observations prior to rollup
+
+# yellow-rumped warbler observations after rollup
+
+
+# Filter to region and season ----
+
+
+# complete checklists in june from the last 10 years
+# filter the checklist data
+
+# filter the observation data
+
+
+# Zero-fill eBird data ----
+
+# combine checklist and observation data to produce detection/non-detection data
+
+# function to convert time observation to hours since midnight
+time_to_decimal <- function(x) {
+  x <- hms(x, quiet = TRUE)
+  hour(x) + minute(x) / 60 + second(x) / 3600
+}
+# transform effort variables
+# 1. converet counts to integer and "X" to NA
+# 2. set distance to 0 for stationary checklists
+# 3. convert duration to hours
+# 4. create speed variable
+# 5. convert time to hours since midnight
+# 6. split date into year and day of year
+
+
+# Apply effort filters ----
+
+# traveling or stationary counts with fewer than 10 observers
+# duration <= 6 h, length <= 10 km, speed <= 100km/h
+
+
+
+# Test-train split ----
+
+# split checklists into 20/80 test/train
+
+# remove unnecessary colors
+checklists <- zf_split %>%
+  select(checklist_id, observer_id, type,
+         observation_count, species_observed,
+         state_code, locality_id, latitude, longitude,
+         protocol_type, all_species_reported,
+         observation_date, year, day_of_year,
+         hours_of_day,
+         effort_hours, effort_distance_km, effort_speed_kmph,
+         number_observers)
+# save dataset for use in next lesson
+write_csv(checklists, "data/checklists-zf_woothr_june_us-ga.csv", na = "")
+
+
+# Exploratory analysis ----
+
+# load and project gis data to albers equal area conic projection
+map_proj <- st_crs("ESRI:102003")
+ne_land <- read_sf("data/gis-data.gpkg", "ne_land") %>%
+  st_transform(crs = map_proj) %>%
+  st_geometry()
+ne_country_lines <- read_sf("data/gis-data.gpkg", "ne_country_lines") %>%
+  st_transform(crs = map_proj) %>%
+  st_geometry()
+ne_state_lines <- read_sf("data/gis-data.gpkg", "ne_state_lines") %>%
+  st_transform(crs = map_proj) %>%
+  st_geometry()
+ga_boundary <- read_sf("data/gis-data.gpkg", "ne_states") %>%
+  filter(state_code == "US-GA") %>%
+  st_transform(crs = map_proj) %>%
+  st_geometry()
+
+# prepare ebird data for mapping
+checklists_sf <- checklists %>%
+  # convert to spatial points
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>%
+  st_transform(crs = map_proj) %>%
+  select(species_observed)
+
+# map
+par(mar = c(0.25, 0.25, 4, 0.25))
+# set up plot area
+plot(st_geometry(checklists_sf),
+     main = "Wood Thrush eBird Observations\nJune 2014-2023",
+     col = NA, border = NA)
+# contextual gis data
+plot(ne_land, col = "#cfcfcf", border = "#888888", lwd = 0.5, add = TRUE)
+plot(ga_boundary, col = "#e6e6e6", border = NA, add = TRUE)
+plot(ne_state_lines, col = "#ffffff", lwd = 0.75, add = TRUE)
+plot(ne_country_lines, col = "#ffffff", lwd = 1.5, add = TRUE)
+# ebird observations
+# not observed
+plot(filter(checklists_sf, !species_observed),
+     pch = 19, cex = 0.1, col = alpha("#555555", 0.25),
+     add = TRUE)
+# observed
+plot(filter(checklists_sf, species_observed),
+     pch = 19, cex = 0.3, col = alpha("#4daf4a", 1),
+     add = TRUE)
+# legend
+legend("bottomright", bty = "n",
+       col = c("#555555", "#4daf4a"),
+       legend = c("eBird checklists", "Wood Thrush sightings"),
+       pch = 19)
+box()
